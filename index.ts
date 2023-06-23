11b7ac00a57309e28b27ecdcc3cacf065da36df6
binerdy <15685383+binerdy@users.noreply.github.com>
2023-06-23 13:33:29 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { of } from 'rxjs';
 import { RequestBuffer } from './recipe/request-buffer';
 
-const requestBuffer = new RequestBuffer(
+new RequestBuffer(
   {
-    test1: of<string>('data'),
+    test1: of('data1'),
+    test2: of('data2'),
+    test3: of('data3'),
+    test4: of('data4'),
+    test5: of('data5'),
   },
   { buffer: 1, retry: 0 }
-);
+)
+  .asObservable()
+  .subscribe(console.log);
--- recipe/request-buffer.ts
+++ recipe/request-buffer.ts
@@ -10,38 +10,40 @@ interface BufferOptions {
   retry: number;
 }
 
-export class RequestBuffer<TData, TRequest extends Record<string, TData[]>> {
+export class RequestBuffer<TRequest extends Record<string, any>> {
   constructor(
     private readonly requests: ObservableData<TRequest>,
     private readonly options: BufferOptions
   ) {}
 
   public toPromise(): Promise<TRequest> {
-    return firstValueFrom(
-      from(Object.entries(this.requests)).pipe(
-        bufferCount(this.options.buffer),
-        reduce(
-          (partialRequest, bufferedRequests) =>
-            this.createPartialRequest(partialRequest, bufferedRequests),
-          {} as Partial<ObservableData<TRequest>>
-        ),
-        concatMap((partialRequest) =>
-          forkJoin(partialRequest).pipe(retry(this.options.retry))
-        ),
-        reduce(
-          (data, bufferedResults) => ({
-            ...data,
-            ...bufferedResults,
-          }),
-          {} as TRequest
-        )
+    return firstValueFrom(this.asObservable());
+  }
+
+  public asObservable(): Observable<TRequest> {
+    return from(Object.entries(this.requests)).pipe(
+      bufferCount(this.options.buffer),
+      reduce(
+        (partialRequest, bufferedRequests) =>
+          this.createPartialRequest(partialRequest, bufferedRequests),
+        {} as Partial<ObservableData<TRequest>>
+      ),
+      concatMap((partialRequest) =>
+        forkJoin(partialRequest).pipe(retry(this.options.retry))
+      ),
+      reduce(
+        (data, bufferedResults) => ({
+          ...data,
+          ...bufferedResults,
+        }),
+        {} as TRequest
       )
     );
   }
 
   private createPartialRequest(
     initialValue: Partial<ObservableData<TRequest>>,
-    bufferedRequests: [string, Observable<TData[]>][]
+    bufferedRequests: [string, Observable<any>][]
   ): Partial<ObservableData<TRequest>> {
     return bufferedRequests.reduce((partialRequest, bufferedRequest) => {
       (partialRequest as any)[bufferedRequest[0]] = bufferedRequest[1];
